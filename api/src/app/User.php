4206ae2c707aed0be71fcbfe8ef352ce6cee0d8d
togana <o.togana@gmail.com>
2020-09-07 19:00:05 +0900

    設定ミスってた

--- api/src/app/User.php
+++ api/src/app/User.php
@@ -72,7 +72,7 @@ class User extends Authenticatable
      */
     public function pushNotifications()
     {
-        return $this->hasMany(PushNotification::class, 'token');
+        return $this->hasMany(PushNotification::class);
     }
 
     /**
